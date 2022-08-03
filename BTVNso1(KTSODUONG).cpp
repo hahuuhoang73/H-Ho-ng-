2a48ac7c53b6577db7edca0932a3f4d24576b149
hahuuhoang73 <110458374+hahuuhoang73@users.noreply.github.com>
2022-08-03 23:45:33 +0700

    BTVNSO1(KTSODUONG)

---
+++ BTVNso1(KTSODUONG).cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(){
+	int a;
+	
+	printf("nhap vao so a: ");
+	scanf("%d", &a);
+	
+	if (a>0){
+		printf("%d la so duong", a);
+	}else{
+		printf("%d khong phai la so duong", a);
+	}
+	}
+	
+	
+	
+	
